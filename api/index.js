782a142971cc26f80b1003f36de44e70379e8a26
Mike Williamson <mike@korora.ca>
2019-05-07 20:09:52 -0400

    Properly import server

--- api/index.js
+++ api/index.js
@@ -1,7 +1,8 @@
-const { server } = require('./src/server')
+const { Server } = require('./src/server')
 const { PORT = 3000 } = process.env
 
-server.listen(PORT, err => {
+
+Server().listen(PORT, err => {
   if (err) throw err
   // eslint-disable-next-line no-console
   console.log(`🚀 API listening on port ${PORT}`)
