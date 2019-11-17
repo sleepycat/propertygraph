f8e4ee26ab3b236a38156553c535cd07963448e0
Mike Williamson <mike@korora.ca>
2019-11-16 19:00:59 -0500

    Create db connection on startup
    This commit uses ArangoTools to set the initial state of the database
    when the server starts.

--- api/index.js
+++ api/index.js
@@ -1,3 +1,4 @@
+const { ArangoTools } = require('arango-tools')
 const { Server } = require('./src/server')
 const {
   PORT = 3000,
@@ -5,9 +6,36 @@ const {
   PROPERTYGRAPH_TEST_DB_URL: url,
 } = process.env
 
+const databaseName = 'propertygraph'
 
-Server().listen(PORT, err => {
-  if (err) throw err
-  // eslint-disable-next-line no-console
-  console.log(`🚀 API listening on port ${PORT}`)
-})
+;(async () => {
+  const { migrate } = await ArangoTools({ rootPass, url })
+  const { query } = await migrate([
+    {
+      type: 'database',
+      databaseName,
+      users: [{ username: 'root', passwd: rootPass }],
+    },
+    {
+      type: 'documentcollection',
+      databaseName,
+      name: 'emailAddresses',
+    },
+    {
+      type: 'documentcollection',
+      databaseName,
+      name: 'emailContents',
+    },
+    {
+      type: 'edgecollection',
+      databaseName,
+      name: 'communications',
+    },
+  ])
+
+  Server({ query }).listen(PORT, err => {
+    if (err) throw err
+    // eslint-disable-next-line no-console
+    console.log(`🚀 API listening on port ${PORT}`)
+  })
+})()
