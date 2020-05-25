f089c7f39122084579a3de4799ea4f57ad5582fd
Mike Williamson <mike@korora.ca>
2020-05-24 20:33:36 -0400

    Update frontend deps and regen locales for lingui

--- frontend/src/i18n.config.js
+++ frontend/src/i18n.config.js
@@ -1,17 +1,20 @@
 import { i18n } from '@lingui/core'
+import { fr, en } from 'make-plural/plurals'
+i18n.loadLocaleData('en', { plurals: en })
+i18n.loadLocaleData('fr', { plurals: fr })
 
 export async function activate(locale) {
   let catalog
-
   try {
     catalog = await import(
       /* webpackChunkName: "i18n-[index]" */ `@lingui/loader!./locales/${locale}.po`
     )
   } catch (e) {
     // this fails only during tests due to webpack errors.
+    catalog = {messages: {}}
   }
 
-  i18n.load(locale, catalog)
+  i18n.load(locale, catalog.messages)
   i18n.activate(locale)
 }
 
